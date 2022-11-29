1c63fe6fbb669878c3d3d31fc4f51b8fd208824a
Burhan AYDIN <aydinburhan@outlook.com.tr>
2022-11-29 13:46:32 +0300

    Routing

---
+++ src/components/NotFount.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFount() {
+  return (
+    <div>
+        <h3>404 - ERROR</h3>
+        <p>ARADIĞINIZ SAYFA BULUNAMADI VEYA YAYINDAN KALDIRILMIŞ OLABİLİR.!!!</p>
+    </div>
+  )
+}
